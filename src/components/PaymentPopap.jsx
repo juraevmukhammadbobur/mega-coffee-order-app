3b3e90de70695d9bd8d0f8554d5e876de2634e2c
Googler969 <juraevboburjon73@gmail.com>
2025-03-26 12:13:55 +0900

    Fixed #3

--- src/components/PaymentPopap.jsx
+++ src/components/PaymentPopap.jsx
@@ -2,7 +2,7 @@ function PaymentPopap() {
   console.log("popap");
   return (
     <div className="flex top-0 left-0 w-full h-full bg-amber-300">
-      <h1>Payment</h1>
+      <h1>Paymenteed</h1>
     </div>
   );
 }
