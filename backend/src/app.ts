ec799fa45cb342caf5aad43c4b187456e25e283f
juraevboburjon <juraevboburjon73@gmail.com>
2025-09-03 12:06:14 +0900

    config prisma

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import menuRoutes from "./api/menu/menu.routes";
 
 const app = express();
 
@@ -9,5 +10,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+app.use("/api/menu", menuRoutes);
 
 export default app;
