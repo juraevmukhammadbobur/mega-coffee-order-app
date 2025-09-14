342677b1c12e53f4578e5ec4a0f7f9d07a28a369
juraevboburjon <juraevboburjon73@gmail.com>
2025-09-14 13:10:19 +0900

    conf vjson

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,11 @@ import orderRoutes from "./api/orders/orders.routes";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
